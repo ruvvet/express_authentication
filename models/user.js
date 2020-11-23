c073ec877b53efcf2b9745bde6edf9b5cd0b29f2
Jenny <finej01@gmail.com>
2020-11-23 12:11:34 -0500

    updated model validation + hashing

--- models/user.js
+++ models/user.js
@@ -57,3 +57,37 @@ module.exports = (sequelize, DataTypes) => {
   );
   return user;
 };
+
+// HASH THE USER PASSWORD
+// add a method
+
+// addhook is a sequelize method that adds a method
+// 'beforecreate' means >>> do it before its added to the table
+
+user.addHook('beforeCreate', (pendingUser) => {
+  //now bcrypt can hash the pw
+  // call the hashsync function and pass in the user
+  //hashSync ([input], [# of times its encrypted])
+  // more than 12 encryptions will take a long time
+  // 12x hash is safe + efficient
+  let hash = bcrypt.hashSync(pendingUser.password, 12);
+
+  //then set pw to equal the hashed version
+  pendingUser.password = hash;
+});
+
+// validate password
+user.prototype.validPassword = function (passwordTyped) {
+  let correctPassword = bcrypt.compareSync(passwordTyped, this.password);
+
+  return correctPassword;
+};
+
+// remove the pw before its userData is serialized/returned for any call
+// only removes pw from request, not from the DB itself
+user.prototype.toJSON = function (){
+
+  let userData = this.get();
+  delete userData.password;
+  return userData;
+}
